7c4c6b9aa3100a1562bd0585db8cb813c9c45f6a
MiyueSC <913784771@qq.com>
2021-04-07 22:05:36 +0800

    fix: 拖拽位置修正

--- src/components/hooks/dropEventHook.ts
+++ src/components/hooks/dropEventHook.ts
@@ -4,7 +4,8 @@ export default function DropEventHook() {
   const store = useStore();
 
   const drop: any = (event: MouseEvent) => {
-    const { offsetX, offsetY, target } = event;
+    // @ts-ignore
+    const { offsetX, offsetY, target, path } = event;
 
     console.log(event);
 
@@ -15,13 +16,16 @@ export default function DropEventHook() {
     const draggedConfig = store.state.draggedConfig || {};
 
     let left, top;
-    if ((target as HTMLElement).classList.contains('cp')) {
-      // 拖拽放置的目标位置在组件上
-      left = (target as HTMLElement).offsetLeft + offsetX - (draggedConfig?.offsetX || 0);
-      top = (target as HTMLElement).offsetTop + offsetY - (draggedConfig?.offsetY || 0);
-    } else {
+
+    // 拖放结束时鼠标在画布上
+    if ((target as HTMLElement).tagName === "DIV" && (target as HTMLElement).className === "editor-canvas-area") {
       left = offsetX - (draggedConfig?.offsetX || 0);
       top = offsetY - (draggedConfig?.offsetY || 0);
+    } else {
+      let targetDiv = (path as HTMLElement[]).find((el: HTMLElement) => el.className.indexOf("cp cp") !==-1);
+      // 拖拽放置的目标位置在组件上
+      left = (targetDiv as HTMLElement).offsetLeft + offsetX - (draggedConfig?.offsetX || 0);
+      top = (targetDiv as HTMLElement).offsetTop + offsetY - (draggedConfig?.offsetY || 0);
     }
     // 边界判定
     if (left < 0) left = 0;
