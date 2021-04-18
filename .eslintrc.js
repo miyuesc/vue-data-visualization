a3b419e56a0b772542ab472cf9a8060dfec6ad7a
MiyueSC <913784771@qq.com>
2021-04-18 16:48:44 +0800

    1

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,29 @@
 module.exports = {
   root: true,
   env: {
-    node: true,
+    node: true
   },
-  extends: [
-    'plugin:vue/vue3-essential',
-    '@vue/typescript/recommended',
-    "@vue/prettier"
-  ],
+  extends: ['plugin:vue/vue3-essential', '@vue/typescript/recommended', '@vue/prettier'],
   parserOptions: {
-    ecmaVersion: 2020,
+    ecmaVersion: 2020
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     'prettier/prettier': [
-      'warn', {
+      'warn',
+      {
         useTabs: false, // 不使用tab
         singleQuote: true, // 不使用单引号
         printWidth: 120, // 换行字符串阈值
         semi: true, // 句末加分号
-        trailingComma: "none", // 最后一个对象元素加逗号
+        trailingComma: 'none', // 最后一个对象元素加逗号
         bracketSpacing: true, // 对象，数组加空格
         jsxBracketSameLine: true, // jsx > 是否另起一行
-        arrowParens: "avoid", // (x) => {} 是否要有小括号
+        arrowParens: 'avoid', // (x) => {} 是否要有小括号
         requirePragma: false, // 是否要注释来决定是否格式化代码
-        proseWrap: "preserve" // 是否要换行
+        proseWrap: 'preserve' // 是否要换行
       }
     ]
   }
